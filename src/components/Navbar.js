81470cb98b8ce1cbb5844f7bba3eb95fa550b654
Zetzher <j.abasolo.agenjo@gmail.com>
2020-12-13 12:11:02 +0100

    done history

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,15 @@ class Navbar extends Component {
   render() {
     return (
       <>
-        <nav style={{position: 'absolute', display: 'flex', alignItems: 'center', backgroundColor: 'white', opacity:'0.8', paddingRight: 50, zIndex: 10}}>
+        <nav className="navbar">
           <Link to="/" style={{marginRight: 30}}>
             <img src={logo} alt="logo" style={{width: 50, height: 50}} />
           </Link>
-          <Link to="/aboutme" style={{marginRight: 30}}>About us</Link>
-          <Link to="/projects" style={{marginRight: 30}}>Projects</Link>
-          <Link to="/history" style={{marginRight: 30}}>History</Link>
-          <Link to="/unpartnership" style={{marginRight: 30}}>Unpartnership</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/aboutme" style={{marginRight: 30, textDecoration: 'none'}}><h1>ABOUT US</h1></Link>
+          <Link to="/projects" style={{marginRight: 30, textDecoration: 'none'}}><h1>PROJECTS</h1></Link>
+          <Link to="/history" style={{marginRight: 30, textDecoration: 'none'}}><h1>HISTORY</h1></Link>
+          <Link to="/unpartnership" style={{marginRight: 30, textDecoration: 'none'}}><h1>UNPARTNERSHIP</h1></Link>
+          <Link to="/contact" style={{textDecoration: 'none'}}><h1>CONTACT</h1></Link>
         </nav>
       </>
     );
--- src/pages/History.js
+++ src/pages/History.js
@@ -1,11 +1,111 @@
-import React from 'react'
+import React from "react";
+import HistoryPlanet from "../assets/gifs/History-Planet.gif";
+import HistorySemen from "../assets/gifs/History-Sperm.gif";
+import HistoryAmeba from "../assets/gifs/History-Amoeba.gif";
+import HistoryCloud from "../assets/gifs/History-Cloud.gif";
+import HistoryDino from "../assets/gifs/History-Dino.gif";
+import HistoryNatureEarth from "../assets/gifs/History-Nature_Earth.gif";
 
 let History = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+  return (
+    <>
+      <section className="history-container-padre">
+        <div className="history-first-section">
+          <article className="box-gradient">
+            <h1>
+              4.600 - 4.400 MYA
+              <br /> BIG BANG
+            </h1>
+            <h2>
+              Creation of earth and its initial outer layer. First models of
+              oceans, continents and our star products: volcanoes.
+            </h2>
+            <img src={HistoryNatureEarth} alt="gif-planet" className="nature-earth-history" />
+          </article>
+          <div></div>
+          <article className="box-gradient">
+            <h1>
+              1.500 MYA
+              <br /> OXYGEN
+            </h1>
+            <h2>
+              Oxygen is released for the first time to critical acclaim and
+              mostly positive reviews
+            </h2>
+            <img src={HistoryCloud} alt="gif-planet" className="history-cloud" />
+          </article>
+          <div></div>
+          <article className="box-gradient">
+            <h1>
+              225 MYA
+              <br /> DINOSAURS
+            </h1>
+            <h2>
+              Evolution plugin is tried on lizards for first time resulting in
+              our now retired beloved product: Dinosaurs
+            </h2>
+            <img src={HistoryDino} alt="gif-planet" className="history-dino" />
+          </article>
+          <div></div>
+          <article className="box-gradient">
+            <h1>
+              0.13 MYA
+              <br /> HUMAN RELEASE
+            </h1>
+            <h2>Homo sapiens are deployed in Africa to worldwide acclaim</h2>
+            <img src={HistorySemen} alt="gif-planet" className="history-semen" />
+          </article>
+          <div></div>
+        </div>
+        <div className="history-second-section">
+          <article className="box-gradient">
+            <h1>
+              3.850 MYA
+              <br /> CREATION OF LIFE
+            </h1>
+            <h2>
+              First successful delivery of life through single cell organisms.
+              Full-cycle release
+            </h2>
+            <img src={HistoryAmeba} alt="gif-planet" className="history-ameba" />
+          </article>
+          <div></div>
+          <article className="box-gradient">
+            <h1>
+              750 - 350 MYA
+              <br /> EVOLUTION PLUGIN
+            </h1>
+            <h2>
+              Release of first evolution plugin affecting: Plants, fish, single
+              celled animals, and land vertebrates
+            </h2>
+          </article>
+          <div></div>
+          <article className="box-gradient">
+            <h1>
+              65 MYA
+              <br /> MAJOR RECALL
+            </h1>
+            <h2>
+              Dinosaur product is recalled due to lack of hability to survivie
+              meteors
+            </h2>
+          </article>
+          <div></div>
+          <aside>
+            <article className="box-gradient">
+              <h1>2020 CURRENT SHITDOWN</h1>
+              <h2>
+                Totally preventable environmental catastrophe. Impending human
+                recall.
+              </h2>
+            </article>
+            <img src={HistoryPlanet} alt="gif-planet" className="history-planet" />
+          </aside>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default History;
